5254fabebba8628f9df2bca4fef5eb5a648f76ef
ravaneswaran <ravaneswaran@gmail.com>
2021-05-01 13:35:44 +0530

    admin configuration home included

--- src/main/java/com/techsocialist/TempleSitesApplication.java
+++ src/main/java/com/techsocialist/TempleSitesApplication.java
@@ -2,12 +2,20 @@ package com.techsocialist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TempleSitesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempleSitesApplication.class, args);
+	}
+
+	@GetMapping("/config")
+	public String siteConfigHome(){
+		return "Admin Configuration Home";
 	}
 
 }
