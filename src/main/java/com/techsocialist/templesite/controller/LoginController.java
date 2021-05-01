a643126381a443c527f95b78d091c7649a415543
ravaneswaran <ravaneswaran@gmail.com>
2021-05-02 00:58:28 +0530

    login and logout controller included

--- src/main/java/com/techsocialist/templesite/controller/LoginController.java
+++ src/main/java/com/techsocialist/templesite/controller/LoginController.java
@@ -1,2 +1,16 @@
-package com.techsocialist.templesite.controller;public class LoginController {
+package com.techsocialist.templesite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(
+            @RequestParam(name="userName", required=false, defaultValue="")String userName,
+            @RequestParam(name="password", required=false, defaultValue="")String password){
+        return null;
+    }
 }
--- src/main/java/com/techsocialist/templesite/controller/LogoutController.java
+++ src/main/java/com/techsocialist/templesite/controller/LogoutController.java
@@ -1,2 +1,13 @@
-package com.techsocialist.templesite.controller;public class LogoutController {
+package com.techsocialist.templesite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout(){
+        return null;
+    }
 }
