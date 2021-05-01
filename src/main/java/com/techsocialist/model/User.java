89c82ce318707531d9b06fd8dd00b92a7f3fe5d7
ravaneswaran <ravaneswaran@gmail.com>
2021-05-01 17:41:40 +0530

    datatbase access implemented

--- src/main/java/com/techsocialist/model/User.java
+++ src/main/java/com/techsocialist/model/User.java
@@ -1,2 +1,125 @@
-package com.techsocialist.model;public class User {
+package com.techsocialist.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    private String uuid;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private String password;
+
+    private String emailId;
+
+    private String mobileNo;
+
+    private String uniqueId;
+
+    private String status;
+
+    private Date createdDate;
+
+    private Date modifiedDate;
+
+    public User(){
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
