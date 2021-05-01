cd6f6178ff5df727e5a4e0ecaaaca78742d6abf2
ravaneswaran <ravaneswaran@gmail.com>
2021-05-01 14:45:44 +0530

    config handlers included

---
+++ src/main/java/com/techsocialist/TempleSiteConfiguration.java
@@ -0,0 +1,36 @@
+package com.techsocialist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TempleSiteConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempleSiteConfiguration.class, args);
+	}
+
+	@GetMapping("/configuration")
+	public String configuration(){
+		return "Admin Configuration Home";
+	}
+
+	@GetMapping("/configuration/site")
+	public String siteConfiguration(){
+		return "Site Configuration Home";
+	}
+
+	@GetMapping("/configuration/site/admin")
+	public String siteAdminConfiguration(){
+		return "Site admin Configuration Home";
+	}
+
+	@GetMapping("/configuration/site/theme")
+	public String siteThemeConfiguration(){
+		return "Site theme Configuration Home";
+	}
+
+}
