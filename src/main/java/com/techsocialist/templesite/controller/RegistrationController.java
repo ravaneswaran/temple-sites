dfd252822d8fa607fdfe34873b6c8aaf9cea9a3d
ravaneswaran <ravaneswaran@gmail.com>
2021-05-02 00:33:43 +0530

    controllers included

--- src/main/java/com/techsocialist/templesite/controller/RegistrationController.java
+++ src/main/java/com/techsocialist/templesite/controller/RegistrationController.java
@@ -1,2 +1,38 @@
-package com.techsocialist.templesite.controller;public class RegistrationController {
+package com.techsocialist.templesite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/registration/site")
+    public String registerSite(
+            @RequestParam(name="siteName", required=false, defaultValue="")String siteName,
+            @RequestParam(name="status", required=false, defaultValue="")String status,
+            @RequestParam(name="createdDate", required=false, defaultValue="")Date createdDate,
+            @RequestParam(name="modifiedDate", required=false, defaultValue="")Date modifiedDate){
+        return null;
+    }
+
+    @GetMapping("/registration/site-admin")
+    public String registerSiteAdmin(
+            @RequestParam(name="site", required=false, defaultValue="")String site,
+            @RequestParam(name="firstName", required=false, defaultValue="")String firstName,
+            @RequestParam(name="lastName", required=false, defaultValue="")String lastName,
+            @RequestParam(name="emailId", required=false, defaultValue="")String emailId,
+            @RequestParam(name="password", required=false, defaultValue="")String password,
+            @RequestParam(name="confirmPassword", required=false, defaultValue="")String confirmPassword,
+            @RequestParam(name="mobileNo", required=false, defaultValue="")String mobileNo,
+            @RequestParam(name="uniqueId", required=false, defaultValue="")String uniqueId,
+            @RequestParam(name="role", required=false, defaultValue="")String role,
+            @RequestParam(name="status", required=false, defaultValue="")String status,
+            @RequestParam(name="createdDate", required=false, defaultValue="")Date createdDate,
+            @RequestParam(name="modifiedDate", required=false, defaultValue="")Date modifiedDate){
+        return null;
+    }
+
 }
